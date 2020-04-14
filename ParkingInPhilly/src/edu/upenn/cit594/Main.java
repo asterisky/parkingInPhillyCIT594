9f5e6f3277d81eb2b106783b565dee8ec6260e72
Katie Pizziketti <kpizziketti@gmail.com>
2020-04-14 13:08:22 -0400

    Method stub and update package for Main.

---
+++ ParkingInPhilly/src/edu/upenn/cit594/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.upenn.cit594;
+
+/**
+ * @author quetzalcoatl
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
