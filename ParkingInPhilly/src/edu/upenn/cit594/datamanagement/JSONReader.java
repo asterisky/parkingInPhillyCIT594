9679ca0aa0eb7f558279bcfaa96ee92edc414898
Katie Pizziketti <kpizziketti@gmail.com>
2020-04-14 13:23:14 -0400

    Added JSONReader class stub to read in JSON files.

---
+++ ParkingInPhilly/src/edu/upenn/cit594/datamanagement/JSONReader.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package edu.upenn.cit594.datamanagement;
+
+import java.util.*;
+/**
+ * @author Aman Nischal and Katie Pizziketti
+ * Methods to read in data from OpenDataPhilly in JSON file format.
+ * Some data cleaning is also included when importing data.
+ *
+ */
+public class JSONReader implements Reader {
+
+	@Override
+	public Set<String> readFile(String filename){
+		return null;
+	}
+}
