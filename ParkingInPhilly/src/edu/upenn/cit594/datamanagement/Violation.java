2515ad68e05e0ab60b19bfac856837c544004c61
Katie Pizziketti <kpizziketti@gmail.com>
2020-04-16 11:48:03 -0400

    Getters/setters and formatting for Violations class

--- ParkingInPhilly/src/edu/upenn/cit594/datamanagement/Violation.java
+++ ParkingInPhilly/src/edu/upenn/cit594/datamanagement/Violation.java
@@ -2,15 +2,16 @@
  * 
  */
 package edu.upenn.cit594.datamanagement;
+
 import java.time.*;
 
 /**
- * @author Aman Nischal and Katie Pizziketti
- * This object holds the data for a parking violation.
+ * @author Aman Nischal and Katie Pizziketti This object holds the data for a
+ *         parking violation.
  *
  */
 public class Violation {
-	
+
 	protected LocalDateTime dateTime;
 	protected int fine;
 	protected String violation;
@@ -18,8 +19,10 @@ public class Violation {
 	protected String state;
 	protected int ticketID;
 	protected int zip;
-	
-	public Violation(LocalDateTime dateDatum, int fineDatum, String vioDatum, int carDatum, String stateDatum, int ticDatum, int zipDatum) {
+
+	//constructor for one row of data on parking violations
+	public Violation(LocalDateTime dateDatum, int fineDatum, String vioDatum, int carDatum, String stateDatum,
+			int ticDatum, int zipDatum) {
 		this.dateTime = dateDatum;
 		this.fine = fineDatum;
 		this.violation = vioDatum;
@@ -27,6 +30,64 @@ public class Violation {
 		this.state = stateDatum;
 		this.ticketID = ticDatum;
 		this.zip = zipDatum;
+	}
+	
+	//it's all getters and setters down here
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public int getFine() {
+		return fine;
+	}
+
+	public void setFine(int fine) {
+		this.fine = fine;
+	}
+
+	public String getViolation() {
+		return violation;
+	}
+
+	public void setViolation(String violation) {
+		this.violation = violation;
+	}
+
+	public int getVehicleID() {
+		return vehicleID;
+	}
+
+	public void setVehicleID(int vehicleID) {
+		this.vehicleID = vehicleID;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getTicketID() {
+		return ticketID;
+	}
+
+	public void setTicketID(int ticketID) {
+		this.ticketID = ticketID;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
 	}
 
 }
