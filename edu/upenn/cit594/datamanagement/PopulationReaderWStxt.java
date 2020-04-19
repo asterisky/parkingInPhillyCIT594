8051bf6ce1075ea559fca2fb548f81e95e878c5c
Katie Pizziketti <kpizziketti@gmail.com>
2020-04-19 17:53:38 -0400

    Created new class for parsing population data from text file.

---
+++ edu/upenn/cit594/datamanagement/PopulationReaderWStxt.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package edu.upenn.cit594.datamanagement;
+
+/**
+ * @author Aman Nischal and Katie Pizziketti reads population data from a
+ *         whitespace-separated text document.
+ *
+ */
+public class PopulationReaderWStxt {
+
+}
